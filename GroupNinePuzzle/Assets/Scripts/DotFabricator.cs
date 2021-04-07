c04580b19f19f2bc5b8fbe133abc4d0c12fa4f55
Thomas Monrad Laursen <79590520+thomasmonradlaursen@users.noreply.github.com>
2021-04-07 14:27:56 +0200

    Added local experiments from other projects

---
+++ GroupNinePuzzle/Assets/Scripts/DotFabricator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotFabricator : MonoBehaviour
+{
+    
+    int numberOfDots = 20;
+    public GameObject prefabDot = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        while(numberOfDots > 0)
+        {
+            Debug.Log("Made dot");
+        Vector3 positionVector = generateRandomPosition();
+        Instantiate(prefabDot, positionVector, Quaternion.identity);
+        numberOfDots--;
+        }  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           
+    }
+
+    private Vector3 generateRandomPosition()
+    {
+        Vector3 result;
+        
+        result.x = Random.Range(-6.0f, 6.0f);
+        result.y = Random.Range(-4.0f, 4.0f);
+        result.z = 0.0f;
+        
+        return result;
+    }
+}
---
+++ GroupNinePuzzle/Assets/Scripts/DragNDrop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DragNDrop : MonoBehaviour
+{
+    
+    bool selected;
+
+    private void OnMouseDown() {
+        if(selected)
+        {selected = false;}
+        else
+        {selected = true;}   
+    }
+
+    private void Update() {
+        if(selected){
+            
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+
+        }
+    }  
+    
+}
