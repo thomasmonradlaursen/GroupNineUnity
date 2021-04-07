98e848a65be3a70f2642bf470b3b031530f4f17e
Thomas Monrad Laursen <79590520+thomasmonradlaursen@users.noreply.github.com>
2021-04-07 17:00:21 +0200

    Restored DotFabricator

--- GroupNinePuzzle/Assets/Scripts/DotFabricator.cs
+++ GroupNinePuzzle/Assets/Scripts/DotFabricator.cs
@@ -1,38 +1,38 @@
-﻿public class DotFabricator : MonoBehaviour
-{
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-    int numberOfDots = 20;
+public class DotFabricator : MonoBehaviour
+{
+    
 
     static int xLength = 6;
     static int yLength = 4;
-    int boardDots = xLength * yLength;
+    int boardDots = xLength*yLength;
     public GameObject prefabDot = null;
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        while (numberOfDots > 0)
 
-            Debug.Log("Number of dots along x-axis: " + xLength);
+        Debug.Log("Number of dots along x-axis: " + xLength);
         Debug.Log("Number of dots along x-axis: " + yLength);
         Debug.Log("Number of dots on the board: " + boardDots);
 
         // Generates the points along the x-axis (top/bottom)
-        for (int i = 0; i < xLength; i++)
-        {
-
+        for(int i = 0; i<xLength; i++){
+            
             Vector3 positionVectorBottom = generateXAxisPointsAtRandom(-4.0f);
             Instantiate(prefabDot, positionVectorBottom, Quaternion.identity);
 
             Vector3 positionVectorTop = generateXAxisPointsAtRandom(4.0f);
             Instantiate(prefabDot, positionVectorTop, Quaternion.identity);
-
+            
         }
 
         // Generates the points along the y-axis (left/right)
-        for (int i = 0; i < yLength; i++)
-        {
-
+        for(int i = 0; i<yLength; i++){
+            
             Vector3 positionVectorLeft = generateYAxisPointsAtRandom(-6.0f);
             Instantiate(prefabDot, positionVectorLeft, Quaternion.identity);
 
@@ -43,47 +43,51 @@
 
 
         // Generates the points on the board
-        while (boardDots > 0)
+        while(boardDots > 0)
         {
-            Debug.Log("Made dot");
-            Vector3 positionVector = generateRandomPosition();
-            Instantiate(prefabDot, positionVector, Quaternion.identity);
-            numberOfDots--;
             Vector3 positionVector = generateBoardPointsAtRandom();
             Instantiate(prefabDot, positionVector, Quaternion.identity);
             boardDots--;
-        }
+        }  
     }
 
-@ -26,7 +57,29 @@ public class DotFabricator : MonoBehaviour
-
-
+    // Update is called once per frame
+    void Update()
+    {
+           
     }
 
-private Vector3 generateRandomPosition()
     private Vector3 generateXAxisPointsAtRandom(float yPosition)
-{
-    Vector3 result;
+    {
+        Vector3 result;
+        
+        result.x = Random.Range(-6.0f, 6.0f);
+        result.y = yPosition;
+        result.z = 0.0f;
+        
+        return result;
+    }
 
-    result.x = Random.Range(-6.0f, 6.0f);
-    result.y = yPosition;
-    result.z = 0.0f;
-
-    return result;
+    private Vector3 generateYAxisPointsAtRandom(float xPosition)
+    {
+        Vector3 result;
+        
+        result.x = xPosition;
+        result.y = Random.Range(-4.0f, 4.0f);
+        result.z = 0.0f;
+        
+        return result;
+    }
+    
+    private Vector3 generateBoardPointsAtRandom()
+    {
+        Vector3 result;
+        
+        result.x = Random.Range(-6.0f, 6.0f);
+        result.y = Random.Range(-4.0f, 4.0f);
+        result.z = 0.0f;
+        
+        return result;
+    }
 }
-
-private Vector3 generateYAxisPointsAtRandom(float xPosition)
-{
-    Vector3 result;
-
-    result.x = xPosition;
-    result.y = Random.Range(-4.0f, 4.0f);
-    result.z = 0.0f;
-
-    return result;
-}
-
-private Vector3 generateBoardPointsAtRandom()
-{
-    Vector3 result;
         
