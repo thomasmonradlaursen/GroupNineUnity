ace3d5e9071b0e267c33ce2c224059cfb551a880
Thomas Monrad Laursen <79590520+thomasmonradlaursen@users.noreply.github.com>
2021-04-30 18:31:59 +0200

    Update VerticesGenerator.cs

--- GroupNinePuzzle/Assets/Scripts/VerticesGenerator.cs
+++ GroupNinePuzzle/Assets/Scripts/VerticesGenerator.cs
@@ -6,9 +6,9 @@ public class VerticesGenerator : MonoBehaviour
 {
     
     static int length = 8;
-    static int heigth = 3;
-    public static int verticalVertices = 17;
-    public static int horizontalVertices = 3;
+    static int heigth = 8;
+    public static int verticalVertices = 16;
+    public static int horizontalVertices = 16;
     Vector3[] vertices = new Vector3[verticalVertices*horizontalVertices];
     public GameObject prefabDot = null;
 
