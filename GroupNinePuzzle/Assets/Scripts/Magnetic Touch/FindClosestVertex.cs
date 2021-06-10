38f994fe1e2fa7763b24cc4b211b7d8894f39545
GustavNP <79592064+GustavNP@users.noreply.github.com>
2021-06-10 14:40:48 +0200

    Snap into place kindof working
    Rotation and translation works but not completely as intended.

--- GroupNinePuzzle/Assets/Scripts/Magnetic Touch/FindClosestVertex.cs
+++ GroupNinePuzzle/Assets/Scripts/Magnetic Touch/FindClosestVertex.cs
@@ -32,10 +32,10 @@ public class FindClosestVertex : MonoBehaviour
         Vector3[] verticesOfSelectedPiece = selectedPiece.GetComponent<MeshFilter>().mesh.vertices;
         float smallestDistance = Vector3.Distance(closestVertexInOtherPiece, verticesOfSelectedPiece[0]);
         int indexForClosestVertex = -1;
-        for(int index = 0; index < verticesOfSelectedPiece.Length; index++)
+        for (int index = 0; index < verticesOfSelectedPiece.Length; index++)
         {
             float currentDistance = Vector3.Distance(closestVertexInOtherPiece, verticesOfSelectedPiece[index]);
-            if(smallestDistance <= currentDistance)
+            if (smallestDistance <= currentDistance)
             {
                 indexForClosestVertex = index;
                 smallestDistance = currentDistance;
@@ -43,28 +43,149 @@ public class FindClosestVertex : MonoBehaviour
         }
         return indexForClosestVertex;
     }
+
+    public int FindIndexOfVertexInPiece(GameObject selectedPiece, Vector3 vertex)
+    {
+        Vector3[] verticesOfSelectedPiece = selectedPiece.GetComponent<MeshFilter>().mesh.vertices;
+        int indexForVertex = -1;
+        for (int index = 0; index < verticesOfSelectedPiece.Length; index++)
+        {
+            if (verticesOfSelectedPiece[index] == vertex)
+            {
+                indexForVertex = index;
+            }
+        }
+        return indexForVertex;
+    }
+
     public Vector3 CalculateDisplacementForSnap(GameObject selectedPiece, GameObject magnetPiece, int indexForVertexOfSelected, int indexForVertexOfMagnet)
     {
         Vector3 selectedVertex = selectedPiece.GetComponent<MeshFilter>().mesh.vertices[indexForVertexOfSelected];
         Vector3 magnetVertex = magnetPiece.GetComponent<MeshFilter>().mesh.vertices[indexForVertexOfMagnet];
-        Debug.Log(selectedVertex);
-        Debug.Log(magnetVertex);
-        return selectedVertex-magnetVertex;
+        // Debug.Log(selectedVertex);
+        // Debug.Log(magnetVertex);
+        return selectedVertex - magnetVertex;
+    }
+
+    public float CalculateRotationForSnap(Vector3 primaryVertexInSelected, Vector3 primaryVertexInPieceToSnapTo, Vector3 previousVertexInSelected, Vector3 previousVertexInPieceToSnapTo)
+    {
+        // Vector3 selectedVertex = selectedPiece.GetComponent<MeshFilter>().mesh.vertices[indexForVertexOfSelected];
+        // Vector3 magnetVertex = magnetPiece.GetComponent<MeshFilter>().mesh.vertices[indexForVertexOfMagnet];
+        // Vector3 selectedPreviousVertex = sel.GetComponent<MeshFilter>().mesh.vertices[indexOfVertexToRotateAbout];
+
+        Vector3 lineToVertexInSelected = previousVertexInSelected - primaryVertexInSelected;
+        Vector3 lineToVertexInPieceToSnapTo = previousVertexInPieceToSnapTo - primaryVertexInPieceToSnapTo;
+
+        Vector3 unitVectorSelected = lineToVertexInSelected / Vector3.Magnitude(lineToVertexInSelected);
+        Vector3 unitVectorInPieceToSnapTo = lineToVertexInPieceToSnapTo / Vector3.Magnitude(lineToVertexInPieceToSnapTo);
+
+
+        var radiansToRotate = 0f;
+        if (unitVectorSelected != unitVectorInPieceToSnapTo)
+        {
+            Debug.Log("unitVectorSelected != unitVectorInPieceToSnapTo");
+            // radiansToRotate = Mathf.Atan2(unitVectorInPieceToSnapTo.y - unitVectorSelected.y, unitVectorInPieceToSnapTo.x - unitVectorSelected.x);
+            var radiansSelected = Mathf.Atan2(unitVectorSelected.y, unitVectorSelected.x);
+            var radiansPieceToSnapTo = Mathf.Atan2(unitVectorInPieceToSnapTo.y, unitVectorInPieceToSnapTo.x);
+            Debug.Log("radiansSelected " + radiansSelected * Mathf.Rad2Deg);
+            Debug.Log("radiansPieceToSnapTo " + radiansPieceToSnapTo * Mathf.Rad2Deg);
+
+
+            radiansToRotate = radiansPieceToSnapTo - radiansSelected;
+        }
+        // radiansToRotate = Mathf.Atan2(lineToVertexInPieceToSnapTo.y - lineToVertexInSelected.y, lineToVertexInPieceToSnapTo.x - lineToVertexInSelected.x);
+        // var radiansPieceToSnapTo = Mathf.Atan2(lineToVertexInPieceToSnapTo.y, lineToVertexInPieceToSnapTo.x);
+
+        Debug.Log("lines and rotation:");
+        Debug.Log(lineToVertexInSelected);
+        Debug.Log(lineToVertexInPieceToSnapTo);
+
+        // var radiansToRotate = radiansPieceToSnapTo - radiansSelected;
+        // Debug.Log("radiansSelected " + radiansSelected);
+        // Debug.Log("radiansPieceToSnapTo " + radiansPieceToSnapTo);
+        Debug.Log("radiansToRotate " + radiansToRotate * Mathf.Rad2Deg);
+
+        var test = Mathf.Atan2(0.5f, -0.5f);
+
+        Debug.Log("radians test " + test * Mathf.Rad2Deg);
+
+        return radiansToRotate;
     }
     public void CalculateVerticesAfterSnapTranslation(GameObject selectedPiece, Vector3 displacement)
     {
         Mesh meshForSelectedPiece = selectedPiece.GetComponent<MeshFilter>().mesh;
         LineRenderer lineRenderer = selectedPiece.GetComponent<LineRenderer>();
         Vector3[] translatedVertices = new Vector3[meshForSelectedPiece.vertices.Length];
-        for(int index = 0; index < meshForSelectedPiece.vertices.Length; index++)
+        for (int index = 0; index < meshForSelectedPiece.vertices.Length; index++)
         {
             translatedVertices[index].x = meshForSelectedPiece.vertices[index].x - displacement.x;
             translatedVertices[index].y = meshForSelectedPiece.vertices[index].y - displacement.y;
+            Debug.Log("index: " + index + "   x: " + translatedVertices[index].x + "   y:" + translatedVertices[index].y);
         }
         meshForSelectedPiece.SetVertices(translatedVertices);
         lineRenderer.SetPositions(translatedVertices);
         selectedPiece.GetComponent<MeshCollider>().sharedMesh = meshForSelectedPiece;
     }
+
+    public void CalculateVerticesAfterSnapRotation(GameObject selectedPiece, float rotationAngle, Vector3 vertexToRotateAbout)
+    {
+        // Mesh meshForSelectedPiece = selectedPiece.GetComponent<MeshFilter>().mesh;
+        // LineRenderer lineRenderer = selectedPiece.GetComponent<LineRenderer>();
+        // Vector3[] translatedVertices = new Vector3[meshForSelectedPiece.vertices.Length];
+        // for (int index = 0; index < meshForSelectedPiece.vertices.Length; index++)
+        // {
+        //     translatedVertices[index].x = meshForSelectedPiece.vertices[index].x - displacement.x;
+        //     translatedVertices[index].y = meshForSelectedPiece.vertices[index].y - displacement.y;
+        //     Debug.Log("index: " + index + "   x: " + translatedVertices[index].x + "   y:" + translatedVertices[index].y);
+        // }
+        // meshForSelectedPiece.SetVertices(translatedVertices);
+        // lineRenderer.SetPositions(translatedVertices);
+        // selectedPiece.GetComponent<MeshCollider>().sharedMesh = meshForSelectedPiece;
+
+        Mesh meshForSelectedPiece = selectedPiece.GetComponent<MeshFilter>().mesh;
+        LineRenderer lineRenderer = selectedPiece.GetComponent<LineRenderer>();
+        Vector3[] rotatedVertices = new Vector3[meshForSelectedPiece.vertices.Length];
+        Vector3[] originalVertices = meshForSelectedPiece.vertices;
+
+        // Vector3 centroid = selectedPiece.GetComponent<PieceInfo>().centroid;
+        originalVertices = CentralizeVertices(vertexToRotateAbout, originalVertices);
+        float rotationTheta = rotationAngle;
+        for (int index = 0; index < originalVertices.Length; index++)
+        {
+            rotatedVertices[index].x = originalVertices[index].x * Mathf.Cos(rotationTheta) - originalVertices[index].y * Mathf.Sin(rotationTheta);
+            rotatedVertices[index].y = originalVertices[index].x * Mathf.Sin(rotationTheta) + originalVertices[index].y * Mathf.Cos(rotationTheta);
+        }
+        rotatedVertices = RestorePositionOfVertices(vertexToRotateAbout, rotatedVertices);
+        meshForSelectedPiece.SetVertices(rotatedVertices);
+        lineRenderer.SetPositions(rotatedVertices);
+        selectedPiece.GetComponent<MeshCollider>().sharedMesh = meshForSelectedPiece;
+
+    }
+
+    // Todo: already exists in Rotation.cs
+    Vector3[] CentralizeVertices(Vector3 centroid, Vector3[] originalVertices)
+    {
+        for (int index = 0; index < originalVertices.Length; index++)
+        {
+            // we add 100000 because we want all coordinates to be positive.
+            originalVertices[index].x = originalVertices[index].x - centroid.x;
+            originalVertices[index].y = originalVertices[index].y - centroid.y;
+        }
+        return originalVertices;
+    }
+
+    // Todo: already exists in Rotation.cs
+    Vector3[] RestorePositionOfVertices(Vector3 centroid, Vector3[] rotatedVertices)
+    {
+        for (int index = 0; index < rotatedVertices.Length; index++)
+        {
+            // we add 100000 because we want all coordinates to be positive.
+            rotatedVertices[index].x = rotatedVertices[index].x + centroid.x;
+            rotatedVertices[index].y = rotatedVertices[index].y + centroid.y;
+        }
+        return rotatedVertices;
+    }
+
     // In case a piece can snap to more than one piece at once, select the closest and snap to it.
     public (GameObject, int, float) SelectPieceToSnapTo(List<(GameObject, Vector3, int, float)> closestVerticesToSelectedPiece)
     {
@@ -80,7 +201,7 @@ public class FindClosestVertex : MonoBehaviour
                 distanceForClosestVertex = currentVertex.Item4;
             }
         }
-        Debug.LogFormat("Closest : {0}. Index for vertex: {1}. Distance: {2}.", pieceWithClosestVertex.name, indexForVertex, distanceForClosestVertex);
+        // Debug.LogFormat("Closest : {0}. Index for vertex: {1}. Distance: {2}.", pieceWithClosestVertex.name, indexForVertex, distanceForClosestVertex);
         return (pieceWithClosestVertex, indexForVertex, distanceForClosestVertex);
     }
     public void LogVertexAndDistance(List<(GameObject, Vector3, int, float)> closestVerticesToSelectedPiece)
@@ -90,8 +211,143 @@ public class FindClosestVertex : MonoBehaviour
             Debug.LogFormat("For : {0}. Vertex at index: {1} Value of vertex: {2}.", closestVertex.Item1.name, closestVertex.Item3, closestVertex.Item2);
         }
     }
-    int GetWrappingIndex(int index, int lengthOfArray)
+    public int GetWrappingIndex(int index, int lengthOfArray)
     {
         return ((index % lengthOfArray) + lengthOfArray) % lengthOfArray;
+    }
+
+    public SnapInformation FindClosestVertexToSelectedPiece((GameObject, List<GameObject>) possibleSnaps)
+    {
+        List<(GameObject, Vector3, int, float)> closestVerticesToSelectedPiece = new List<(GameObject, Vector3, int, float)>();
+        Vector3[] verticesOfSelected = possibleSnaps.Item1.GetComponent<MeshFilter>().mesh.vertices;
+        (float, float) smallestDistance = (100000f, 100000f);
+        Vector3 primaryVertexInSelectedPiece = new Vector3();
+        Vector3 primaryVertexInPieceToSnapTo = new Vector3();
+        Vector3 previousVertexInSelectedPiece = new Vector3();
+        Vector3 previousVertexInPieceToSnapTo = new Vector3();
+
+        int indexOfPrimaryVertexInSelectedPiece = 0;
+        int indexOfPrimaryVertexInPieceToSnapTo = 0;
+        int indexOfPreviousVertexInSelectedPiece = 0;
+        int indexOfPreviousVertexInPieceToSnapTo = 0;
+
+        GameObject pieceWithShortestDistance = null;
+        foreach (GameObject candidateForSnap in possibleSnaps.Item2)
+        {
+            Vector3[] verticesOfCandicate = candidateForSnap.GetComponent<MeshFilter>().mesh.vertices;
+            // float distanceFromClosestVertexToSelectedPiece = Vector3.Distance(verticesOfCandicate[0], baseVertex);
+            // Vector3 clostestVertexForCurrentPiece = verticesOfCandicate[0];
+            // int indexForVertex = -1;
+            // for (int index = 0; index < verticesOfCandicate.Length; index++)
+            // {
+            //     float distanceFromCurrentVertex = Vector3.Distance(verticesOfCandicate[index], baseVertex);
+            //     if (distanceFromCurrentVertex <= distanceFromClosestVertexToSelectedPiece)
+            //     {
+            //         clostestVertexForCurrentPiece = verticesOfCandicate[index];
+            //         distanceFromClosestVertexToSelectedPiece = distanceFromCurrentVertex;
+            //         indexForVertex = index;
+            //     }
+            // }
+            // closestVerticesToSelectedPiece.Add((candidateForSnap, clostestVertexForCurrentPiece, indexForVertex, distanceFromClosestVertexToSelectedPiece));
+            var result = CheckDistancesBetweenVerticesOfTwoPieces(verticesOfSelected, verticesOfCandicate);
+
+
+            //Not correct. smallest distance might be item2, if the distance between previous vertice and other piece is smaller than distance between next vertex and other piece.
+            if (result.Item1.Item1 < smallestDistance.Item1)
+            {
+                smallestDistance = result.Item1;
+                primaryVertexInPieceToSnapTo = result.Item2.Item1.Item2;
+                primaryVertexInSelectedPiece = result.Item2.Item1.Item1;
+                previousVertexInPieceToSnapTo = result.Item2.Item2.Item2;
+                previousVertexInSelectedPiece = result.Item2.Item2.Item1;
+                indexOfPrimaryVertexInSelectedPiece = result.Item3.Item1.Item1;
+                indexOfPrimaryVertexInPieceToSnapTo = result.Item3.Item1.Item2;
+                indexOfPreviousVertexInSelectedPiece = result.Item3.Item2.Item1;
+                indexOfPreviousVertexInPieceToSnapTo = result.Item3.Item2.Item2;
+                pieceWithShortestDistance = candidateForSnap;
+            }
+        }
+
+        // Debug.Log("DistanceBetweenPrimaryVertices: " + smallestDistance.Item2);
+        // Debug.Log("DistanceBetweenPreviousVertices: " + smallestDistance.Item1);
+        // Debug.Log("PrimaryVertexInSelectedPiece: " + primaryVertexInSelectedPiece);
+        // Debug.Log("PrimaryVertexInPieceToSnapTo: " + primaryVertexInPieceToSnapTo);
+        // Debug.Log("PreviousVertexInSelectedPiece: " + previousVertexInSelectedPiece);
+        // Debug.Log("PreviousVertexInPieceToSnapTo: " + previousVertexInPieceToSnapTo);
+
+        var snapInformation = new SnapInformation()
+        {
+            PieceToSnapTo = pieceWithShortestDistance,
+            DistanceBetweenPreviousVertices = smallestDistance.Item1,
+            DistanceBetweenPrimaryVertices = smallestDistance.Item2,
+            PrimaryVertexInPieceToSnapTo = primaryVertexInPieceToSnapTo,
+            PrimaryVertexInSelectedPiece = primaryVertexInSelectedPiece,
+            PreviousVertexInPieceToSnapTo = previousVertexInPieceToSnapTo,
+            PreviousVertexInSelectedPiece = previousVertexInSelectedPiece,
+            IndexOfPrimaryVertexInSelectedPiece = indexOfPrimaryVertexInSelectedPiece,
+            IndexOfPrimaryVertexInPieceToSnapTo = indexOfPrimaryVertexInPieceToSnapTo,
+            IndexOfPreviousVertexInSelectedPiece = indexOfPreviousVertexInSelectedPiece,
+            IndexOfPreviousVertexInPieceToSnapTo = indexOfPreviousVertexInPieceToSnapTo,
+        };
+
+        return snapInformation;
+    }
+
+
+    public ((float, float), ((Vector3, Vector3), (Vector3, Vector3)), ((int, int), (int, int))) CheckDistancesBetweenVerticesOfTwoPieces(Vector3[] piece1, Vector3[] piece2)
+    {
+
+        // item1 is distance from vertice i in piece1 to vertice k in piece2.
+        // item2 is distance from vertice i+1 in piece1 to vertice k+1 in piece2.
+        (float, float) smallestDistance = (100000f, 100000f);
+
+        // Previous vertix in piece1 paired with previous vertex in piece2
+        // This is then paired with the current vertex in piece1 paired with the current vertex in piece2
+        ((Vector3, Vector3), (Vector3, Vector3)) verticesInShortestDistance = ((new Vector3(), new Vector3()), (new Vector3(), new Vector3()));
+
+        ((int, int), (int, int)) indexesOfVerticesInShortestDistance = ((0, 0), (0, 0));
+
+        for (int i = 0; i < piece1.Length; i++)
+        {
+            var vertex_piece1 = piece1[i];
+            var previousVertex_piece1 = piece1[GetWrappingIndex(i - 1, piece1.Length)];
+            var nextVertex_piece1 = piece1[GetWrappingIndex(i + 1, piece1.Length)];
+            for (int k = 0; k < piece2.Length; k++)
+            {
+                var vertex_piece2 = piece2[k];
+                var previousVertex_piece2 = piece2[GetWrappingIndex(k + 1, piece2.Length)]; // + because we need to go the other way around compared to piece1.
+                var nextVertex_piece2 = piece2[GetWrappingIndex(k - 1, piece2.Length)]; // - because the same thing.
+
+                var distBetweenVertices = Vector3.Distance(vertex_piece1, vertex_piece2);
+                if (distBetweenVertices < smallestDistance.Item1)
+                {
+                    var distPreviousVertices = Vector3.Distance(previousVertex_piece1, previousVertex_piece2);
+                    var distNextVertices = Vector3.Distance(nextVertex_piece1, nextVertex_piece2);
+
+                    if (distPreviousVertices <= distNextVertices)
+                    {
+                        smallestDistance = (distBetweenVertices, distPreviousVertices);
+                        verticesInShortestDistance = ((vertex_piece1, vertex_piece2), (previousVertex_piece1, previousVertex_piece2));
+                        indexesOfVerticesInShortestDistance = ((i, k), (GetWrappingIndex(i - 1, piece1.Length), GetWrappingIndex(k + 1, piece2.Length)));
+                    }
+                    else
+                    {
+                        smallestDistance = (distBetweenVertices, distNextVertices);
+                        verticesInShortestDistance = ((vertex_piece1, vertex_piece2), (nextVertex_piece1, nextVertex_piece2));
+                        indexesOfVerticesInShortestDistance = ((i, k), (GetWrappingIndex(i + 1, piece1.Length), GetWrappingIndex(k - 1, piece2.Length)));
+                    }
+                }
+            }
+        }
+
+        return (smallestDistance, verticesInShortestDistance, indexesOfVerticesInShortestDistance);
+    }
+
+    float DistanceBetweenVertices(Vector3 vertex1, Vector3 vertex2)
+    {
+        var xDist = vertex2.x - vertex1.x;
+        var yDist = vertex2.y - vertex1.y;
+        var distance = Mathf.Sqrt(Mathf.Pow(xDist, 2) + Mathf.Pow(yDist, 2));
+        return distance;
     }
 }
--- GroupNinePuzzle/Assets/Scripts/Magnetic Touch/MagneticTouchAlgorithm.cs
+++ GroupNinePuzzle/Assets/Scripts/Magnetic Touch/MagneticTouchAlgorithm.cs
@@ -29,21 +29,41 @@ public class MagneticTouchAlgorithm : MonoBehaviour
 
     void FindClosestVertexPair()
     {
-        List<(GameObject, Vector3, int, float)> closestVerticesToSelectedPiece = new List<(GameObject, Vector3, int, float)>();
-        Vector3 centroidOfSelectedPiece = possibleSnaps.Item1.GetComponent<PieceInfo>().centroid;
-        closestVerticesToSelectedPiece = GetComponentInParent<FindClosestVertex>().FindClosestVertexToCentroidOfSelectedPiece(possibleSnaps, centroidOfSelectedPiece);
-        GetComponentInParent<FindClosestVertex>().LogVertexAndDistance(closestVerticesToSelectedPiece);
-        (GameObject, int, float) pieceWithClosestVertex;
-        pieceWithClosestVertex = GetComponentInParent<FindClosestVertex>().SelectPieceToSnapTo(closestVerticesToSelectedPiece);
-        Vector3 closestVertexOfMagnet = pieceWithClosestVertex.Item1.GetComponent<MeshFilter>().mesh.vertices[pieceWithClosestVertex.Item2];
+        // List<(GameObject, Vector3, int, float)> closestVerticesToSelectedPiece = new List<(GameObject, Vector3, int, float)>();
+        // Vector3 centroidOfSelectedPiece = possibleSnaps.Item1.GetComponent<PieceInfo>().centroid;
+        // closestVerticesToSelectedPiece = GetComponentInParent<FindClosestVertex>().FindClosestVertexToCentroidOfSelectedPiece(possibleSnaps, centroidOfSelectedPiece);
+        // GetComponentInParent<FindClosestVertex>().LogVertexAndDistance(closestVerticesToSelectedPiece);
+
+        SnapInformation snapInformation;
+        snapInformation = GetComponentInParent<FindClosestVertex>().FindClosestVertexToSelectedPiece(possibleSnaps);
+        // pieceWithClosestVertex = GetComponentInParent<FindClosestVertex>().SelectPieceToSnapTo(closestVerticesToSelectedPiece);
+        // Vector3 closestVertexOfMagnet = pieceWithClosestVertex.Item1.GetComponent<MeshFilter>().mesh.vertices[pieceWithClosestVertex.Item2];
         GameObject selectedPiece = GetComponentInParent<MeshFromJsonGenerator>().selectedObject;
+
         if (this.name.Equals(selectedPiece.name))
         {
-            int closestVertexInSelectedPiece = GetComponentInParent<FindClosestVertex>().FindClosestVertexInSelectedPiece(selectedPiece, closestVertexOfMagnet);
-            Debug.LogFormat("Closest vertex in selected piece: " + selectedPiece.GetComponent<MeshFilter>().mesh.vertices[closestVertexInSelectedPiece]);
-            Vector3 displacement = GetComponentInParent<FindClosestVertex>().CalculateDisplacementForSnap(selectedPiece, pieceWithClosestVertex.Item1, closestVertexInSelectedPiece, pieceWithClosestVertex.Item2);
+            // snapInformation.DebugLogInformation();
+            // Debug.Log(selectedPiece.name);
+            // Debug.Log("this: " + this.name);
+            // Debug.Log("this.name.Equals(selectedPiece.name");
+            // int closestVertexInSelectedPiece = GetComponentInParent<FindClosestVertex>().FindClosestVertexInSelectedPiece(selectedPiece, closestVertexOfMagnet);
+            
+            // Todo: maybe return index in SnapInformation result instead of finding it in this kinda odd way.
+            // int indexOfClosestVertexInSelectedPiece = GetComponentInParent<FindClosestVertex>().FindClosestVertexInSelectedPiece(selectedPiece, snapInformation.PrimaryVertexInPieceToSnapTo);
+            // int indexOfClosestVertexInPieceToSnapTo = GetComponentInParent<FindClosestVertex>().FindClosestVertexInSelectedPiece(snapInformation.PieceToSnapTo, snapInformation.PrimaryVertexInSelectedPiece);
+            int indexOfClosestVertexInSelectedPiece = snapInformation.IndexOfPrimaryVertexInSelectedPiece;
+            int indexOfClosestVertexInPieceToSnapTo = snapInformation.IndexOfPrimaryVertexInPieceToSnapTo;
+            int indexOfNeighborVertexInSelectedPiece = snapInformation.IndexOfPreviousVertexInSelectedPiece;
+            int indexOfNeighborVertexInPieceToSnapTo = snapInformation.IndexOfPreviousVertexInPieceToSnapTo;
+            // int indexOfClosestVertexInPieceToSnapTo = GetComponentInParent<FindClosestVertex>().FindIndexOfVertexInPiece(snapInformation.PieceToSnapTo, snapInformation.PrimaryVertexInPieceToSnapTo);
+            // Debug.LogFormat("index of vertex in selected piece: " + indexOfClosestVertexInSelectedPiece);
+            // Debug.LogFormat("vertex in selected piece: " + selectedPiece.GetComponent<MeshFilter>().mesh.vertices[indexOfClosestVertexInSelectedPiece]);
+            Vector3 displacement = GetComponentInParent<FindClosestVertex>().CalculateDisplacementForSnap(selectedPiece, snapInformation.PieceToSnapTo, indexOfClosestVertexInSelectedPiece, indexOfClosestVertexInPieceToSnapTo);
+            float rotation = GetComponentInParent<FindClosestVertex>().CalculateRotationForSnap(snapInformation.PrimaryVertexInSelectedPiece, snapInformation.PrimaryVertexInPieceToSnapTo, snapInformation.PreviousVertexInSelectedPiece, snapInformation.PreviousVertexInPieceToSnapTo);
             Debug.Log("Displacement: " + displacement);
+            Debug.Log("Rotation: " + rotation);
             GetComponentInParent<FindClosestVertex>().CalculateVerticesAfterSnapTranslation(selectedPiece, displacement);
+            GetComponentInParent<FindClosestVertex>().CalculateVerticesAfterSnapRotation(selectedPiece, rotation, snapInformation.PrimaryVertexInSelectedPiece);
         }
     }
 
@@ -62,13 +82,13 @@ public class MagneticTouchAlgorithm : MonoBehaviour
         float maximumForSelected = boundBoxForSelectedPiece.Item2;
         float minimumForCompare;
         float maximumForCompare;
-        Debug.Log("MinimumForSelected: " + minimumForSelected + ", MaximumForSelected: " + maximumForSelected);
+        // Debug.Log("MinimumForSelected: " + minimumForSelected + ", MaximumForSelected: " + maximumForSelected);
         foreach (GameObject piece in pieces)
         {
             (float, float) boundBoxForNext = ConstructBoundBox(piece, margin);
             minimumForCompare = boundBoxForNext.Item1;
             maximumForCompare = boundBoxForNext.Item2;
-            Debug.Log("Minimum: " + minimumForCompare + ", maximum: " + maximumForCompare);
+            // Debug.Log("Minimum: " + minimumForCompare + ", maximum: " + maximumForCompare);
             if (minimumForSelected < maximumForCompare && maximumForSelected > minimumForCompare)
             {
                 if (!(piece.name.Equals(GetComponentInParent<MeshFromJsonGenerator>().selected)))
@@ -81,7 +101,7 @@ public class MagneticTouchAlgorithm : MonoBehaviour
 
     void LogPossibleSnaps()
     {
-        Debug.Log("Number of possible snaps " + possibleSnaps.Item2.Count);
+        // Debug.Log("Number of possible snaps " + possibleSnaps.Item2.Count);
         if (possibleSnaps.Item2.Count > 0)
         {
             string printString = "";
